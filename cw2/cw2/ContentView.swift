a26ef84d6009add6fbd5cd51675682209794fe24
Ahmad Alhouti <ahmadalh3500@gmail.com>
2022-03-13 19:26:33 +0300

    i changed my name

---
+++ cw2/cw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw2
+//
+//  Created by Ahmad Al Houti on 13/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Ahmad")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw2/cw2/cw2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw2App.swift
+//  cw2
+//
+//  Created by Ahmad Al Houti on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
